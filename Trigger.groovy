a84232243bc28c732a51218f1515cd24164209cc
farrukh90 <farrukhsadykov@gmail.com>
2020-03-31 18:49:04 -0500

    Create Trigger.groovy

---
+++ Trigger.groovy
@@ -0,0 +1,18 @@
+node {
+	properties([pipelineTriggers([cron('* * * * *')])])
+	stage("Stage1"){
+		echo "hello"
+}
+	stage("Stage2"){
+		echo "hello"
+}
+	stage("Stage3"){
+		echo "hello"
+}
+	stage("Stage4"){
+		echo "hello"
+}
+	stage("Stage5"){
+		echo "hello"
+	}
+}
