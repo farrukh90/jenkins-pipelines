014b4a1d7ec23e5e21aa212e99fe8c1d5b8c69cf
farrukh90 <farrukhsadykov@gmail.com>
2020-03-31 19:47:24 -0500

    added properties

---
+++ from_scratch.groovy
@@ -0,0 +1,20 @@
+node {
+	properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([cron('* * * * *')])])
+
+
+	stage("Stage1"){
+		echo "hello"
+}
+	stage("Stage2"){
+		echo "hello"
+}
+	stage("Stage3"){
+		echo "hello"
+}
+	stage("Stage4"){
+		echo "hello"
+}
+	stage("Stage5"){
+		echo "hello"
+	}
+}
