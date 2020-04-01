80deef3cec19a07835257b30944ba9b437fa9290
farrukh90 <farrukhsadykov@gmail.com>
2020-03-31 20:26:04 -0500

    added to dev

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -11,20 +11,20 @@ node {
 	}
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@jenkins_worker1.acirrustech.com                 sudo yum install httpd -y
+		ssh centos@dev1.acirrustech.com                 sudo yum install httpd -y
 		"""
 	}
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@jenkins_worker1.acirrustech.com:/tmp
-		ssh centos@jenkins_worker1.acirrustech.com                 sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@jenkins_worker1.acirrustech.com                 sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@jenkins_worker1.acirrustech.com				   sudo chown centos:centos /var/www/html/
-		ssh centos@jenkins_worker1.acirrustech.com				   sudo chmod 777 /var/www/html/*
+		scp -r *  centos@dev1.acirrustech.com:/tmp
+		ssh centos@dev1.acirrustech.com                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@dev1.acirrustech.com                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@dev1.acirrustech.com				   sudo chown centos:centos /var/www/html/
+		ssh centos@dev1.acirrustech.com				   sudo chmod 777 /var/www/html/*
 		"""
 	}
 	stage("Restart web server"){
-		sh "ssh centos@jenkins_worker1.acirrustech.com                 sudo systemctl restart httpd"
+		sh "ssh centos@dev1.acirrustech.com                 sudo systemctl restart httpd"
 	}
 	stage("Slack"){
 		slackSend color: '#BADA55', message: 'Hello, World!'
