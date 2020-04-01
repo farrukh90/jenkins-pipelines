1dea2d52365b887ae5590a5c9b4311de6d64f945
farrukh90 <farrukhsadykov@gmail.com>
2020-03-31 20:06:09 -0500

    added centos

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -11,9 +11,9 @@ node {
 }
 	stage("Install Prerequisites"){
 		sh """
-		ssh jenkins_worker1.acirrustech.com sudo yum install httpd -y
-		ssh jenkins_worker1.acirrustech.comsudo cp -r *  /var/www/html/
-		ssh jenkins_worker1.acirrustech.com sudo systemctl start httpd 
+		ssh centos@jenkins_worker1.acirrustech.com         sudo yum install httpd -y
+		ssh centos@jenkins_worker1.acirrustech.com         sudo cp -r *  /var/www/html/
+		ssh centos@jenkins_worker1.acirrustech.com         sudo systemctl start httpd 
 		"""
 }
 	stage("Stage3"){
