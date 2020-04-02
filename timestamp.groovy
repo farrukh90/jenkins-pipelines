5affb41bf23c3ee08f72b681675bc2f1edbdb760
farrukh90 <farrukhsadykov@gmail.com>
2020-04-02 18:28:01 -0500

    added timestamp

---
+++ timestamp.groovy
@@ -0,0 +1,19 @@
+node {
+	stage("Stage1"){
+		echo "hello"
+}
+	stage("Stage2"){
+		echo "hello"
+}
+	stage("Stage3"){
+		echo "hello"
+}
+	stage("Stage4"){
+		echo "hello"
+}
+	stage("Timestamp"){
+		timestamps {
+			echo "hello"
+		}
+	}
+}
