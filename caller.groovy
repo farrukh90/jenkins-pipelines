38a929080413590eefbf56da78b8c431ad75e271
farrukh90 <farrukhsadykov@gmail.com>
2020-04-02 18:49:41 -0500

    added failure

--- caller.groovy
+++ caller.groovy
@@ -4,16 +4,20 @@ node {
 }
 	stage("Stage2"){
 		echo "hello"
+        // Calls another job called "Timestamp"
         build 'Timestamp'
 }
 	stage("Stage3"){
 		echo "hello"
+        // Calls another job called "Script"
         build "Script"
 }
 	stage("Stage4"){
 		build 'Template2'
+        // Calls another job called "Script"
 }
 	stage("Stage5"){
 		echo "hello"
+        build 'non existing'
 	}
 }
