f0f16ed555d73995faa1a2f859a0a6ab9f5ac9e4
farrukh90 <farrukhsadykov@gmail.com>
2020-04-02 18:11:21 -0500

    added a git pull repo

---
+++ pull_repo.groovy
@@ -0,0 +1,17 @@
+node {
+	stage("Stage1"){
+		git 'https://github.com/farrukh90/packer.git'
+}
+	stage("Stage2"){
+		echo "hello"
+}
+	stage("Stage3"){
+		echo "hello"
+}
+	stage("Stage4"){
+		echo "hello"
+}
+	stage("Stage5"){
+		echo "hello"
+	}
+}
