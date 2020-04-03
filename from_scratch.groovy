bdfc8688185c0ed552c2fb47d9fd2300c89a254a
farrukh90 <farrukhsadykov@gmail.com>
2020-04-02 20:03:05 -0500

    added Farrukh Branch

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -16,7 +16,7 @@ node {
 
 		// Pulls a repo from developer
 	stage("Pull Repo"){
-		git   'https://github.com/farrukh90/cool_website.git'
+		checkout([$class: 'GitSCM', branches: [[name: '*/FarrukH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/cool_website.git']]])
 	}
 		//Installs web server on different environment
 	stage("Install Prerequisites"){
