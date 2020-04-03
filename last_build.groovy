c6b1c66c2e09575b90f7077b4e042f7e97bfd487
farrukh90 <farrukhsadykov@gmail.com>
2020-04-02 19:50:58 -0500

    added last build

---
+++ last_build.groovy
@@ -0,0 +1,29 @@
+node {
+    properties([
+        // Keeps only last 5
+        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+        pipelineTriggers([
+            // Runs with "Input" Job
+            upstream('Input'),
+            // Execute every minute
+            cron('* * * * *')])
+        ])
+    ])
+	stage("Stage1"){
+		git 'https://github.com/farrukh90/packer.git'
+}
+	stage("Stage2"){
+		echo "hello"
+}
+	stage("Stage3"){
+		echo "hello"
+}
+	stage("Stage4"){
+		echo "hello"
+}
+	stage("Stage5"){
+		node {
+		// some block
+		} 
+	}
+}
