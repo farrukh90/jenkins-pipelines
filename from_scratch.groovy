fde1bb8e469c279a9ae8e0cada6aaa87b14eb4bd
farrukh90 <farrukhsadykov@gmail.com>
2020-04-04 14:27:07 -0500

    removed node definition

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -1,4 +1,4 @@
-node('linux') {
+node {
 	properties([
 		// Below line sets "Discard Builds more than 5"
 		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
