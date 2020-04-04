dad811191524d939997ad5d2070dc7ee2ede5d51
farrukh90 <farrukhsadykov@gmail.com>
2020-04-04 14:30:58 -0500

    added properties([disableConcurrentBuilds()

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -2,7 +2,7 @@ node {
 	properties([
 		// Below line sets "Discard Builds more than 5"
 		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
-		
+		disableConcurrentBuilds(),
 		// Below line triggers this job every minute
 		pipelineTriggers([pollSCM('* * * * *')]),
 		parameters([choice(choices: [
