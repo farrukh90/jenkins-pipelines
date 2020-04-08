b65e666bc4446e1678efc90e7eb260db2e73391f
farrukh90 <farrukhsadykov@gmail.com>
2020-04-08 01:04:03 -0500

    added more time

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -79,12 +79,23 @@ node {
 	stage("Run Container"){
 		timestamps {
 			ws {
+			    try {
+			        sh '''
+                    #!/bin/bash
+			        IMAGES=$(ssh centos@dev1.acirrustech.com docker ps -aq) 
+			        for i in \$IMAGES; do
+			            ssh centos@dev1.acirrustech.com docker stop \$i
+                        ssh centos@dev1.acirrustech.com docker rm \$i
+			        done 
+                    '''
+			    } catch(e) {
+                    println("Script failed with error: ${e}")
+                }
 				sh '''
 					ssh centos@dev1.acirrustech.com docker run -dti -p 5001:5000 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
-					'''
+				'''
             }
         }
     }
 }
-
 	
