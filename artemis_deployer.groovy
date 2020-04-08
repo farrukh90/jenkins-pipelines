7cf4b8cc229ddf55069f12db988f36854daafb45
farrukh90 <farrukhsadykov@gmail.com>
2020-04-08 00:20:58 -0500

    added more lines

--- artemis_deployer.groovy
+++ artemis_deployer.groovy
@@ -7,7 +7,14 @@ node {
 		'version/0.2', 
 		'version/0.3', 
 		'version/0.4', 
-		'version/0.5'], 
+		'version/0.5',
+		'version/0.6',
+		'version/0.7',
+		'version/0.8',
+		'version/0.9',
+		'version/10',
+	
+		], 
 	description: 'Which version of the app should I deploy? ', 
 	name: 'Version'), 
 	choice(choices: 
