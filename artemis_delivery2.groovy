06d1cecf59825b6ebbdfac4a281c5a3949929908
farrukh90 <farrukhsadykov@gmail.com>
2020-04-09 18:43:45 -0500

    added different environment

--- artemis_delivery2.groovy
+++ artemis_delivery2.groovy
@@ -86,10 +86,10 @@ properties(
 					try {
 						sh '''
 							#!/bin/bash
-							IMAGES=$(ssh centos@dev1.acirrustech.com docker ps -aq) 
+							IMAGES=$(ssh centos@${ENVIR} docker ps -aq) 
 							for i in \$IMAGES; do
-								ssh centos@dev1.acirrustech.com docker stop \$i
-								ssh centos@dev1.acirrustech.com docker rm \$i
+								ssh centos@${ENVIR} docker stop \$i
+								ssh centos@${ENVIR} docker rm \$i
 							done 
 							'''
 					} catch(e) {
@@ -103,7 +103,7 @@ properties(
 		timestamps {
 			ws {
 				sh '''
-					ssh centos@dev1.acirrustech.com docker run -dti -p 5001:5000 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
+					ssh centos@${ENVIR} docker run -dti -p 5001:5000 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
 					'''
             }
         }
