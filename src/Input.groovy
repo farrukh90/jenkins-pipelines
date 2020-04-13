1c5d5c6c58d156768c5bf8aed7e5243cc58f9721
farrukh90 <farrukhsadykov@gmail.com>
2020-04-12 23:28:21 -0500

    added more stage

--- src/Input.groovy
+++ src/Input.groovy
@@ -1,21 +1,22 @@
 def deployer(ENVIR){
-node {
-	stage("Stage1"){
-		echo "hello"
-	}
-	stage("Stage2"){
-		echo "hello"
-	}
-	stage("Stage3"){
-		echo "hello"
-	}
-	stage("Ask for Input"){
-		input 'Should I proceed?'
-	}
-		stage("Stage5"){
+	node {
+		stage("Stage1"){
+			echo "hello"
+		}
+		stage("Stage2"){
+			echo "hello"
+		}
+		stage("Stage3"){
+			echo "hello"
+		}
+		stage("Ask for Input"){
+			input 'Should I proceed?'
+		}
+			stage("Stage5"){
+		}
 	}
 }
-}
+
 
 
 
